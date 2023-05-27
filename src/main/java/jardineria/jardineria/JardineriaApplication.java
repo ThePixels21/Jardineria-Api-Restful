f5264b25c6aa71ef69b4638749cbd426408af81d
ThePixels21 <santigamer521@gmail.com>
2023-05-26 20:41:58 -0500

    Project structure

--- src/main/java/jardineria/jardineria/JardineriaApplication.java
+++ src/main/java/jardineria/jardineria/JardineriaApplication.java
@@ -2,8 +2,9 @@ package jardineria.jardineria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class JardineriaApplication {
 
 	public static void main(String[] args) {
