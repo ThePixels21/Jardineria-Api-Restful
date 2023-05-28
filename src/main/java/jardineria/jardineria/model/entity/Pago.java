01a5039c29aed5f70db01a94a12f1e490041f63e
ThePixels21 <santigamer521@gmail.com>
2023-05-28 16:34:10 -0500

    Pago class created

---
+++ src/main/java/jardineria/jardineria/model/entity/Pago.java
@@ -0,0 +1,35 @@
+package jardineria.jardineria.model.entity;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pago",
+indexes = {@Index(name = "index_forma_pago", columnList = "forma_pago")})
+public class Pago {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_transaccion")
+    private Long id;
+
+    @Column(name = "forma_pago", length = 40, nullable = false)
+    private String formaPago;
+
+    @Column(name = "fecha_pago", nullable = false)
+    private Date fechaPago;
+
+    @Column(nullable = false)
+    private double total;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo", nullable = false)
+    private Cliente cliente;
+
+}
