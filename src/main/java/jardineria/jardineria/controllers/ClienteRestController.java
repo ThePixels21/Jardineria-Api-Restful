a7d1f86b1efc8574ea127a04a9749379d4a3bf4e
ThePixels21 <santigamer521@gmail.com>
2023-05-29 10:49:18 -0500

    Some bugs fixed

--- src/main/java/jardineria/jardineria/controllers/ClienteRestController.java
+++ src/main/java/jardineria/jardineria/controllers/ClienteRestController.java
@@ -29,12 +29,38 @@ public class ClienteRestController {
     }
 
     @PostMapping("clientes")
-    public ResponseEntity<List<Cliente>> saveCliente(@RequestParam Cliente cliente, @RequestParam("repVentasId") Long repVentasId) {
+    public ResponseEntity<List<Cliente>> saveCliente(@RequestParam("nombre") String nombre, String nombreContacto, String apellidoContacto, @RequestParam("telefono") String telefono, @RequestParam("fax") String fax, @RequestParam("direccion1") String direccion1, String direccion2, @RequestParam("ciudad") String ciudad, String region, String pais, String codigoPostal, Long repVentasId, double limiteCredito) {
+        Cliente cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setNombreContacto(nombreContacto);
+        cliente.setApellidoContacto(apellidoContacto);
+        cliente.setTelefono(telefono);
+        cliente.setFax(fax);
+        cliente.setDireccion1(direccion1);
+        cliente.setDireccion2(direccion2);
+        cliente.setCiudad(ciudad);
+        cliente.setRegion(region);
+        cliente.setPais(pais);
+        cliente.setCodigoPostal(codigoPostal);
+        cliente.setLimiteCredito(limiteCredito);
         return clienteService.saveCliente(cliente, repVentasId);
     }
 
-    @PutMapping("clientes/{id}")
-    public ResponseEntity<List<Cliente>> saveCliente(@RequestParam Cliente cliente, @RequestParam("repVentasId") Long repVentasId, @PathVariable Long clienteId) {
+    @PutMapping("clientes/{clienteId}")
+    public ResponseEntity<List<Cliente>> updateCliente(@RequestParam("nombre") String nombre, String nombreContacto, String apellidoContacto, @RequestParam("telefono") String telefono, @RequestParam("fax") String fax, @RequestParam("direccion1") String direccion1, String direccion2, @RequestParam("ciudad") String ciudad, String region, String pais, String codigoPostal, Long repVentasId, double limiteCredito, @PathVariable Long clienteId) {
+        Cliente cliente = new Cliente();
+        cliente.setNombre(nombre);
+        cliente.setNombreContacto(nombreContacto);
+        cliente.setApellidoContacto(apellidoContacto);
+        cliente.setTelefono(telefono);
+        cliente.setFax(fax);
+        cliente.setDireccion1(direccion1);
+        cliente.setDireccion2(direccion2);
+        cliente.setCiudad(ciudad);
+        cliente.setRegion(region);
+        cliente.setPais(pais);
+        cliente.setCodigoPostal(codigoPostal);
+        cliente.setLimiteCredito(limiteCredito);
         return clienteService.updateCliente(cliente, repVentasId, clienteId);
     }
 
